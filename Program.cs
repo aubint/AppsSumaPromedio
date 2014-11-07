5a6c3ffa06de904ff942a7617f177df1bac86b82
aubint <aubint10@gmail.com>
2014-11-07 12:14:05 -0500

    AppsSumaPromedio

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppsSumaPromedio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero1, numero2, numero3, numero4, suma, promedio;
+            Console.WriteLine("SUMA Y PROMEDIO DE 4 NUMEROS");
+
+            Console.WriteLine("Ingrese numero 1: ");
+            numero1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese numero 2: ");
+            numero2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese numero 3: ");
+            numero3 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese numero 4: ");
+            numero4 = float.Parse(Console.ReadLine());
+
+            suma = numero1 + numero2 + numero3 + numero4;
+            Console.WriteLine("La suma de los 4 numeros es: "+suma);
+            promedio = (numero1 + numero2 + numero3 + numero4) / 4;
+            Console.WriteLine("El promedio de los 4 numeros es: " + promedio);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
